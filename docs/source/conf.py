415ff9b67fbf5bb0a294f43b1f0a15b7eea4e848
LiyaNiTa <110767424+LiyaNiTa@users.noreply.github.com>
2024-04-04 22:41:38 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Parul_na_Python'
+copyright = '2024, JV&LN'
+author = 'JV&LN'
 
 release = '0.1'
 version = '0.1.0'
